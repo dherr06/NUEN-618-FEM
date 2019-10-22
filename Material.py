77883176deecc6041f4388f182d0ac95eebe1fe6
dherr06 <52929250+dherr06@users.noreply.github.com>
2019-10-22 09:54:56 -0500

    Update Material.py

--- Material.py
+++ Material.py
@@ -7,8 +7,8 @@ class Material:
     """
     Load material properties
     """
-    def __init__(self,T):
+    def __init__(self):
         def k(T):
             K = 1.5 + (2510/(215+T))
             return K
-        self.k = k(T)
+        self.k = k
