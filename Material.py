e282f947b0df6e1931ca9728ace1b82fe1ec4ebf
dherr06 <52929250+dherr06@users.noreply.github.com>
2019-10-22 09:29:28 -0500

    Update Material.py

--- Material.py
+++ Material.py
@@ -7,29 +7,8 @@ class Material:
     """
     Load material properties
     """
-
-    def __init__(self, MESH, D, Siga, printout=False):
-
-        # checks:
-        try:
-            if len(D) != len(Siga):
-                raise ValueError("D and Siga of different lengths")
-        except ValueError as err:
-            print(err.args)
-            sys.exit(1)
-            
-        # Assign material arrays 
-        self.D = np.asarray(D)
-        self.Siga = np.asarray(Siga)
-        
-        # check of length of D/Siga is compatible with length of the 
-        #   unique entries in mesh.iel2mat
-        imat_unique = np.unique(MESH.iel2mat)       
-        try:
-            if len(imat_unique) != len(D):
-                raise ValueError("imat_unique and D/Siga of different lengths")
-        except ValueError as err:
-            print(err.args)
-            sys.exit(1)
-
-        
+    def __init__(self,T):
+        def k(T):
+            K = 1.5 + (2510/(215+T))
+            return K
+        self.k = k(T)
